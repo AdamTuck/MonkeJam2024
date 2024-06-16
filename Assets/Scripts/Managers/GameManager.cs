a2f82af29225a5b5714a6534b50e0472f9cc6e6f
Adam Tuck <adamtuck@gmail.com>
2024-06-16 11:09:05 -0700

    Sound Update
    - Added day music, night music, and shop music

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -110,6 +110,8 @@ public class GameManager : MonoBehaviour
         currentDay++;
         UIManager.instance.UpdateCurrentDay(currentDay.ToString());
         UIManager.instance.StartDaySplashScreen();
+
+        SoundManager.instance.SetAmbience("dayMusic");
     }
 
     private void DayRunning()
@@ -122,6 +124,7 @@ public class GameManager : MonoBehaviour
 
     private void StormWallRace()
     {
+        SoundManager.instance.SetAmbience("storm");
         StormWall.instance.ShrinkWall();
     }
 
@@ -133,6 +136,7 @@ public class GameManager : MonoBehaviour
         MissionManager.instance.ClearCurrentMissions();
         WeatherMakerDayNightCycleManagerScript.Instance.Speed = 0;
 
+        SoundManager.instance.SetAmbience("shop");
         ChangeState(GameState.NightUpgrade, currentLevel);
     }
 
---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    [Header("Sources")]
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private AudioSource ambientSource, bikeSource;
+    [SerializeField] private AudioClip sirenAmbient, shopAmbient, dayMusic;
+
+    // Singleton
+    public static SoundManager instance;
+    private void Awake()
+    {
+        if (instance && instance != this)
+        {
+            Destroy(instance);
+            return;
+        }
+        instance = this;
+    }
+
+    public void SetAmbience(string ambience)
+    {
+        ambientSource.Stop();
+
+        switch (ambience)
+        {
+            case "storm":
+                ambientSource.PlayOneShot(sirenAmbient);
+                break;
+            case "shop":
+                ambientSource.PlayOneShot(shopAmbient);
+                break;
+            case "dayMusic":
+                ambientSource.clip = dayMusic;
+                ambientSource.Play();
+                break;
+        }
+    }
+}
